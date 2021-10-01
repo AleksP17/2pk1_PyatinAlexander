8c4833557c353a519af345a17b73ab03ebecd5be
Александр Пятин <saschs.puatin@gmail.com>
2021-10-01 13:50:17 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            for (x = 1; x <= 4; x += 0.5)
+            {
+                double y = x + 3 * Math.Pow(x, 2) - 2.5 * Math.Pow(x, 3) +  0.75 * Math.Pow(x, 4);
+                Console.WriteLine("{0:N}\t{1:N}", x, y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
