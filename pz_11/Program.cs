b872f77830eef467855bfd802302b2eb5df5827a
Александр Пятин <saschs.puatin@gmail.com>
2021-11-08 21:09:41 +0500

    test

---
+++ pz_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "ДОЖДЬ МАШИНА ОКНО ДВЕРЬ ДИВАН СТУЛ КОШКА ";
+            var result = str.Split().Count(x => x.Contains('А'));
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+    }
+}
