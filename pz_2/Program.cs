9a48e633f7f1dd8cc21f6ebd315ad916835a4d91
Александр Пятин <saschs.puatin@gmail.com>
2021-12-09 21:37:16 +0500

    test edit

---
+++ pz_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace pz_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            result = Math.Pow(14 + Math.Pow(8 - 1, 1/3), 1/4) / Math.Abs(8 - 14) * (Math.Pow(Math.Sin(Math.PI/4), 2) + Math.Tan(Math.PI/4));
+            Console.WriteLine(result);
+        }
+    }
+}
