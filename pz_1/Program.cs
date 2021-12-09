64b9e71fecef8e69b9d79274bbd3e798aea8d5c4
Александр Пятин <saschs.puatin@gmail.com>
2021-12-09 22:36:03 +0500

    test edit

---
+++ pz_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение b: ");
+            double b = Double.Parse(Console.ReadLine());
+            double c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
