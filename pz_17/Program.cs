43eb6e812390bc73903aa80a604e2ebbd90d3336
Александр Пятин <saschs.puatin@gmail.com>
2021-12-09 20:27:45 +0500

    test edit

--- pz_17/Program.cs
+++ pz_17/Program.cs
@@ -6,23 +6,24 @@ namespace pz_17
 {
     class Program
     {
-       
-            public static int Geom(int n)
+        public static int[] Geom(int n)
+        {
+            int[] A = new int[n];
+            int i;
+            for (i = 0; i < n; i++)
             {
-                if (n == 1)
-                    return 1;
-                else
-                    return Geom(n - 1) * 2;
+                A[i] = 10 * (2^(n - 1));
+                Console.WriteLine(A[i] + " ");
             }
-        
+            return A;
+        }
             
         
         static void Main(string[] args)
         {
+            Console.WriteLine(Geom(4));
             
-            for (int i = 1; i < Geom(4); i++)
-                Console.WriteLine(Geom(i));
-            Console.ReadLine();
+
         }
     }
 }
