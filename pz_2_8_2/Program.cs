618000e9fb8f1e2e69d5ccb8f57ff52a27ededd4
Александр Пятин <saschs.puatin@gmail.com>
2022-04-21 23:33:55 +0500

    2 задание

---
+++ pz_2_8_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pz_2_8_2
+{
+    delegate void MyDel(char v);
+    class Program
+    {
+        public char some;
+
+        class MyClass
+        {
+            public char some;
+
+            public void MyMethod(char v )
+            {
+                some = v;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var array = new MyClass[3] { new MyClass(), new MyClass(), new MyClass() };
+            MyDel del = null;
+            foreach (var item in array)
+            {
+                del += item.MyMethod;
+            }
+            char v = 'D';
+            del(v);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.some);
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
